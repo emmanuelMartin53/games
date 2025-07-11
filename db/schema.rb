ac19f6eef62f7874a090031a706a4b5db062db15
Emmanuel Martin <em.martin456@gmail.com>
2025-07-11 10:43:01 +0200

    migration user_play_game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_10_175011) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_11_081215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_10_175011) do
     t.index ["builder_id"], name: "index_studios_on_builder_id"
   end
 
+  create_table "user_play_games", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_user_play_games_on_game_id"
+    t.index ["user_id"], name: "index_user_play_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +83,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_10_175011) do
   add_foreign_key "games", "studios"
   add_foreign_key "platforms", "builders"
   add_foreign_key "studios", "builders"
+  add_foreign_key "user_play_games", "games"
+  add_foreign_key "user_play_games", "users"
 end
