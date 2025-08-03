f28a43a8afeb8b6d3547c481d3dfb2995ba591db
Emmanuel Martin <em.martin456@gmail.com>
2025-08-03 10:25:59 +0200

    add file _form.html.erb for views games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :studio
   has_many :user_play_games
-  has_many :platform_has_game
-  has_many :platforms, through: :platform_has_game
+  has_many :platform_has_games
+  has_many :platforms, through: :platform_has_games
 
   validates :title, uniqueness: {
       message: "le jeu existe déjà" }, presence: true
