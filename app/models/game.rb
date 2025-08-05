e73976abd671891843b051415da2339f649cf985
Emmanuel Martin <em.martin456@gmail.com>
2025-08-05 17:38:21 +0200

    update

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,7 @@ class Game < ApplicationRecord
 
   belongs_to :studio
   has_many :user_play_games
-  has_many :platform_has_games
+  has_many :platform_has_games, dependent: :destroy
   has_many :platforms, through: :platform_has_games
 
   validates :title, uniqueness: {
